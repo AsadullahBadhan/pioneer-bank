a3a59dfa68ac0172de7d69b19d7559973f7ce41f
AsadullahBadhan <asadullah.badhan@gmail.com>
2021-09-01 03:45:51 +0600

    login area created

---
+++ app.js
@@ -0,0 +1,6 @@
+const loginBtn = document.querySelector('#login-btn');
+const loginArea = document.querySelector('#login-area');
+
+loginBtn.addEventListener('click', () => {
+    loginArea.style.display = 'none';
+})
